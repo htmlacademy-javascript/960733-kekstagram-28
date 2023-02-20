8a5ae35245d9a1219865bf74d55b5da6239bb0cd
Roman Kostenko <kostenko.r.khv@gmail.com>
2023-02-20 15:36:38 +0300

    Задание 7

---
+++ js/functions.js
@@ -0,0 +1,22 @@
+const fitForLength = (str, strLength) => str.length <= strLength;
+
+const polindrom = (str) => {
+  str = str.replaceAll(' ', '').toLowerCase();
+  return str.split('').reverse().join('') === str;
+};
+
+const onlyDigits = (str) => str.replaceAll(/[^0-9]/g, '');
+
+const customPad = (source, count, addition) => {
+  if (source.length >= count) {
+    return source;
+  }
+  const preffixLength = count - source.length;
+  const sample = addition;
+
+  addition = '';
+  while (addition.length < preffixLength - sample.length) {
+    addition += sample;
+  }
+  return sample.slice(0, preffixLength - addition.length) + addition + source;
+};
