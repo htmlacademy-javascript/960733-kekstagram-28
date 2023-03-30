51efe9059064d6f707acc6db4b0faa3f22de0098
Roman Kostenko <kostenko.r.khv@gmail.com>
2023-03-30 15:14:27 +1000

    Перламутровые пуговицы

---
+++ js/filters.js
@@ -0,0 +1,54 @@
+const Filters = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed'
+};
+const MAX_RANDOM_IMAGES = 10;
+const activeButtonStyle = 'img-filters__button--active';
+
+const filtersContainer = document.querySelector('.img-filters');
+const filtersFormElement = document.querySelector('.img-filters__form');
+
+let currentFiler = Filters.DEFAULT;
+
+// Отобразим блок с фильтрами
+filtersContainer.classList.remove('img-filters--inactive');
+
+const getFilterByValue = (filterValue) => {
+  for (const filter in Filters) {
+    if (Filters[filter] === filterValue) {
+      return Filters[filter];
+    }
+  }
+  return undefined;
+};
+
+const setFilterClick = (cb) => {
+  filtersFormElement.addEventListener('click', (evt) => {
+    const newFilter = getFilterByValue(evt.target.id);
+    if (!newFilter || newFilter === currentFiler) {
+      return;
+    }
+    const activeButtonElement = document.querySelector(`.${activeButtonStyle}`);
+    activeButtonElement.classList.remove(activeButtonStyle);
+    evt.target.classList.add(activeButtonStyle);
+    currentFiler = newFilter;
+    cb();
+  });
+};
+
+const sortByDiscussed = (postA, postB) => postB.comments.length - postA.comments.length;
+
+const filterPosts = (posts) => {
+  switch(currentFiler) {
+    case Filters.DEFAULT:
+      return posts;
+    case Filters.RANDOM:
+      return posts.slice(0).sort(() => Math.random() - 0.5).slice(0, MAX_RANDOM_IMAGES);
+    case Filters.DISCUSSED:
+      return posts.slice(0).sort(sortByDiscussed);
+  }
+};
+
+export {setFilterClick};
+export {filterPosts};
